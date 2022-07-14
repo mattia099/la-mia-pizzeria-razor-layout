b5cd51072af318e2f582139295952bdd4874f26c
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-07-14 14:42:58 +0200

    Aggiungere i file di progetto.

---
+++ Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_razor_layout
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(string name, string description, string image, float price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
