86949610c4f39f5183d56f7257400b2f6d156565
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-07-14 15:53:13 +0200

    Esercizi Razor completati

---
+++ Controllers/EserciziRazor.cs
@@ -0,0 +1,16 @@
+﻿using la_mia_pizzeria_razor_layout.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_razor_layout.Controllers
+{
+    public class EserciziRazor : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
